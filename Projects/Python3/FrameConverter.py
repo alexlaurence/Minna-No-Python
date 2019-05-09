526b0e166a26e0d75704fbe68cbf285e36676417
Alexander Laurence <39573687+alexlaurence@users.noreply.github.com>
2019-05-09 23:58:05 +0100

    Update FrameConverter.py

--- Projects/Python3/FrameConverter.py
+++ Projects/Python3/FrameConverter.py
@@ -10,11 +10,11 @@ Last modified: May 2019
 Website: www.celestial.tokyo
 """
 
-n_frames = input('How many frames are there in the film?')
+total_frames = input('How many frames are there in the film?')
 
 print('OK! Let's start converting...')
 
-for frames in range(n_frames):
-  print('Converting {} frame out of {}'.format(str(frame), str(n_frames))
+for frames in range(total_frames):
+  print('Converting {} frame out of {}'.format(str(frame), str(total_frames))
   
 print('Complete!')
