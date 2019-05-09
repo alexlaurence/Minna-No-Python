817fbbeb6cc263bb25c990c9fb479e7dc0bc4589
Alexander Laurence <39573687+alexlaurence@users.noreply.github.com>
2019-05-09 20:15:23 +0100

    Create FrameConverter.py

---
+++ Projects/Python3/FrameConverter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
+
+"""
+This app asks for total number of frames and prints each frame within a ranged loop
+Author: Alexander A. Laurence
+Last modified: May 2019
+Website: www.celestial.tokyo
+"""
+
+n_frames = input('How many frames are there in the film?')
+
+print('OK! Let's start converting...')
+
+for frames in range(n_frames):
+  print('Converting {} frame out of {}'.format(str(frame), str(n_frames))
+  
+print('Complete!')
