db0e2cc0451866a9b327b805c1158c97fcb7c968
Alexander Laurence <39573687+alexlaurence@users.noreply.github.com>
2019-05-09 23:54:04 +0100

    Create BasicLogin.py

---
+++ Projects/Python3/BasicLogin.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3.4
+# -*- coding: utf-8 -*-
+
+"""
+This app calculates the airplane hanger capacity
+for efficient day-to-day running of the airport
+Author: Alexander A. Laurence
+Last modified: January 2019
+Website: www.celestial.tokyo
+"""
+
+import getpass 
+
+# Instantiate The Dictionary
+password_index = {
+     1 : {'Username': 'mrbooleanswag','Password': '1234'},
+     2 : {'Username': 'Lexington','Password': 'kfsdjgfdg'}
+}
+
+# Get User input
+get_username = input('Username: ')
+get_password = getpass.getpass('Password: ')
+
+# Set bools to false
+auth_username = False
+auth_password = False
+
+# Search Dictionary
+i = 1
+while i < len(password_index) + 1:
+  if password_index[i]['Username'] == get_username:
+    auth_username = True
+    
+    j = 1
+    while j < len(password_index) + 1:
+      if password_index[j]['Password'] == get_password:
+        auth_password = True
+        break
+      j += 1
+    break
+  i += 1
+  
+# Authentication Logic
+if auth_username == False:
+  print('Incorrect username.')
+  
+if auth_password == False:
+  print('Incorrect Password')
+  
+if auth_password == False or auth_username == False:
+  print('Failed to log in')
+  
+if auth_password == True and auth_username == True:
+  print('Successfully logged in')
